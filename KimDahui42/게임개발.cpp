252ca12ab3941c5fd81beacc68f9fc25f99a61c8
KimDahui42 <kdie44@gmail.com>
2022-05-24 02:06:00 +0900

    구현

---
+++ KimDahui42/게임개발.cpp
@@ -0,0 +1,74 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    int n, m, facing, cr, cc, cnt = 0;
+    int go_dir[4][2] = {{-1, 0}, {0, 1}, {1, 0}, {0, -1}};
+    int back_dir[4][2] = {{1, 0}, {0, -1}, {-1, 0}, {0, 1}};
+
+    cin >> n >> m;
+    cin >> cr >> cc >> facing;
+    vector<vector<int>> game_map(n, vector<int>(m, 1));
+
+    for (int i = 0; i < n; i++)
+        for (int j = 0; j < m; j++)
+            cin >> game_map[i][j];
+
+    while (true)
+    {
+        int next_face = (facing + 1) % 4;
+        int next_r = cr + go_dir[next_face][0];
+        int next_c = cc + go_dir[next_face][1];
+        if (game_map[next_r][next_c] == 0)
+        {
+            cr = next_r;
+            cc = next_c;
+            facing = next_face;
+            cnt++;
+            continue;
+        }
+        while (next_face != facing)
+        {
+            next_face = (next_face + 1) % 4;
+            next_r = cr + go_dir[next_face][0];
+            next_c = cc + go_dir[next_face][1];
+            if (game_map[next_r][next_c] == 0)
+            {
+                cr = next_r;
+                cc = next_c;
+                facing = next_face;
+                cnt++;
+                break;
+            }
+        }
+        if (next_face == facing)
+        {
+            next_r = cr + back_dir[next_face][0];
+            next_c = cc + back_dir[next_face][1];
+            next_face = (next_face + 1) % 4;
+            while (next_face != facing)
+            {
+                next_face = (next_face + 1) % 4;
+                next_r = cr + go_dir[next_face][0];
+                next_c = cc + go_dir[next_face][1];
+                if (game_map[next_r][next_c] == 0)
+                {
+                    cr = next_r;
+                    cc = next_c;
+                    facing = next_face;
+                    cnt++;
+                    break;
+                }
+            }
+            next_face == facing;
+            break;
+        }
+    }
+    cout << cnt;
+}
--- README.md
+++ README.md
@@ -1,3 +1,11 @@
 # codingtest-study
 
 이것이 코딩테스트다 스터디를 위한 저장소
+
+- 1주차 2022-05-22
+  - ch03. 그리디
+    - <a href="./yejin/Greedy">[yejin] | Greedy</a>
+    - <a href="./KimDahui42/그리디.md">[KimDahui42] | 그리디</a>
+  - ch04. 구현
+    - <a href=""></a>
+    - <a href=""></a>
